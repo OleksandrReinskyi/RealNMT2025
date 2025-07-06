8f6cb8c5f76b924bf7877dc02aada6faa32dca26
OleksandrReinskyi <sasharivne2008@gmail.com>
2025-07-06 16:04:50 +0300

    finished nmt page, started structuring test page

--- static/JS/nmt.js
+++ static/JS/nmt.js
@@ -17,13 +17,24 @@ function sosavUp(event){
     }
 }
 
-instrConfirmationButton.addEventListener("mousedown",sosavDown)
-document.addEventListener("mouseup",sosavUp)
+function wait(ms){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res()
+        },ms)
+    })
+}
 
 let confirmationIndex = 1;
 const testBlocks = document.querySelectorAll("[data-num]")
 
-document.addEventListener("click",(event)=>{
+
+instrConfirmationButton.addEventListener("mousedown",sosavDown)
+document.addEventListener("mouseup",sosavUp)
+
+const main = document.querySelector("main");
+
+document.addEventListener("click",async (event)=>{
     let target = event.target;
     if(target.classList.contains("test__next")){
         testBlocks.forEach((item)=>{
@@ -31,6 +42,29 @@ document.addEventListener("click",(event)=>{
         })
         testBlocks[confirmationIndex].classList.add("active");
         confirmationIndex++;
+        if(confirmationIndex == testBlocks.length){
+
+            await wait(2000);
+            
+            main.classList.add("hidden");
+
+            await wait(750) 
+            main.classList.remove("hidden")
+
+            await wait(250)
+            queueMicrotask(()=>{
+                for(let i = 0; i<250000;i++){
+                    console.log(i)
+                }
+            })
+            
+            await wait(250) 
+            document.querySelector("body").innerHTML = `
+            <h1 id="global-error">Помилка 502: сервер знизу!</h1>
+            `
+            await wait(3000)
+            window.location.href = window.location.href.replace(/\/[^\/]*$/, "/test");
+        }
     }
 
     let imageNum = target.getAttribute("data-imgnum");
